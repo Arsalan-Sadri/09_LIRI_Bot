ba850d053c6e53818bb58eed1ce16e1e672652eb
Arsalan Sadri <asadri62@gmail.com>
2018-09-14 08:57:15 -0700

    ...final version...full functionality... added a new function called buildQueryTerm() to make it more moduar and and also defined queryTerm as a global variable to be accessable to by functions...

--- liri.js
+++ liri.js
@@ -1,20 +1,51 @@
-var doThis = process.argv[2];
-switch (doThis) {
-    case "movie-this":
-        movieThis();
-        break;
-    case "spotify-this-song":
-        spotifyThis()
-        break;
-    case "concert-this":
-        concertThis()
-        break;
-    case "do-what-it-says":
-        break;
-    default:
-        console.log("Not a valid operation!");
-        break;
+var queryTerm = "";
+
+doThis(process.argv[2]);
+
+function doThis(doIt) {
+    switch (doIt) {
+        case "movie-this":
+            movieThis();
+            break;
+        case "spotify-this-song":
+            spotifyThis()
+            break;
+        case "concert-this":
+            concertThis()
+            break;
+        case "do-what-it-says":
+            readFromFile();
+            break;
+        default:
+            console.log("\n******************************\n");
+            console.log("Not a valid operation! Please try again.");
+            console.log("\n******************************\n");
+            break;
+    }
 }
+
+function buildQueryTerm() {
+    for (var i = 3; i < process.argv.length; i++) {
+        if (i > 3 && i < process.argv.length) {
+            queryTerm = queryTerm + "+" + process.argv[i];
+        } else {
+            queryTerm += process.argv[i];
+        }
+    }
+} // End of buildQueryTerm
+
+function readFromFile() {
+    var fs = require("fs");
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log('Error occurred while dealing with file: ' + error);
+        }
+        var dataArr = data.split(",");
+        queryTerm = dataArr[1];
+        doThis(dataArr[0]);
+
+    });
+} // End of readFromFile()
 
 function spotifyThis() {
     require("dotenv").config();
@@ -22,22 +53,17 @@ function spotifyThis() {
     var Spotify = require('node-spotify-api');
     var spotify = new Spotify(spotifyKeys);
 
-    var queryTerm = "Hotel California";
-    for (var i = 3; i < process.argv.length; i++) {
-        if (i > 3 && i < process.argv.length) {
-            queryTerm = queryTerm + "+" + process.argv[i];
-        } else {
-            queryTerm = "";
-            queryTerm += process.argv[i];
-        }
-    }
+    buildQueryTerm();
+    if (queryTerm === "") queryTerm = "Hotel California";
 
     spotify.search({
         type: 'track',
         query: queryTerm
     }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
+        if (err || data.tracks.items[0].artists[0].name === undefined) {
+            console.log("\n******************************\n");
+            console.log("No artist was found! Please try again.");
+            return console.log("\n******************************\n");
         }
 
         //
@@ -53,22 +79,15 @@ function spotifyThis() {
 
 } // End of spotifyThis()
 
-function movieThis(movieName) {
+function movieThis() {
     var request = require("request");
 
-    var movieName = "Mr.+Nobody";
-    for (var i = 3; i < process.argv.length; i++) {
-        if (i > 3 && i < process.argv.length) {
-            movieName = movieName + "+" + process.argv[i];
-        } else {
-            movieName = "";
-            movieName += process.argv[i];
-        }
-    }
+    buildQueryTerm();
+    if (queryTerm === "") queryTerm = "Mr.+Nobody";
 
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    var queryUrl = "http://www.omdbapi.com/?t=" + queryTerm + "&y=&plot=short&apikey=trilogy";
     request(queryUrl, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
+        if (!error && response.statusCode === 200 && JSON.parse(body).Title !== undefined) {
             console.log("\n******************************\n");
             console.log("Title: " + JSON.parse(body).Title);
             console.log("\nYear: " + JSON.parse(body).Year);
@@ -79,15 +98,20 @@ function movieThis(movieName) {
             console.log("\nPlot: " + JSON.parse(body).Plot);
             console.log("\nActors: " + JSON.parse(body).Actors);
             console.log("\n******************************\n");
+        } else {
+            console.log("\n******************************\n");
+            console.log("No such a movie was found! Please try again.");
+            console.log("\n******************************\n");
         }
     });
 } // End of movieThis()
 
 function concertThis() {
     /* 
-    This API needed to send developers "written consent" in order to let us
-    use their API. I just see this at the night of the submission of assignment,
-    so I did not have enough time to get their written consent. For more info, 
+    This API required "written consent" in order to let us
+    use their API. I just see this at the night of the submission of the assignment,
+    so I did not have enough time to get their written consent. I am not quite sure
+    If I could have gotton their consent even within span of a week! For more info, 
     visit: http://www.artists.bandsintown.com/bandsintown-api 
     */
 } // End of concertThis()
