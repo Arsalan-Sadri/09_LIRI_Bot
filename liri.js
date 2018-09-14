5f791276b2f69653cbd92099830b300fb040138a
Arsalan Sadri <asadri62@gmail.com>
2018-09-14 01:09:42 -0700

    ...adding 5 files...building the foundation...coding starts from here...

---
+++ liri.js
@@ -0,0 +1,6 @@
+// to read and set any environment variables with the dotenv package
+require("dotenv").config();
+
+// import keys.js
+var sptKeys = require("./keys.js");
+// var spotify = new Spotify(sptKeys.spotify);
